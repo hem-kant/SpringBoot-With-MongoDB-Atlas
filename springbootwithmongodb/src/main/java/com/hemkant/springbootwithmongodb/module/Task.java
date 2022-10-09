1a0a7120a76b15428ea57d3ac2628a66e2bac256
Hem Kant <hemkant.india@gmail.com>
2022-10-09 16:21:46 +0530

    Spring Boot With MongoDB Atlas

--- springbootwithmongodb/src/main/java/com/hemkant/springbootwithmongodb/module/Task.java
+++ springbootwithmongodb/src/main/java/com/hemkant/springbootwithmongodb/module/Task.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "tasks")
+@Document(collection = "task")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
